d8c2429617f07c04eb388c22d3047cbebb4a8d5a
kakkak444 <gpg@mail.glabyl.com>
2026-07-18 14:55:38 +0900

    change cursor size

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -85,8 +85,8 @@ end)
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_SIZE", "48")
+hl.env("HYPRCURSOR_SIZE", "48")
 
 -- For NVIDIA
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
