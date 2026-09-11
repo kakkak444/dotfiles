2b9829cecf1cec0d28d022177b9dcab9bf41adba
kakkak444 <gpg@mail.glabyl.com>
2026-09-11 12:44:49 +0900

    follow arch wiki of uwsm

--- .config/hypr/hyprland/binding.lua
+++ .config/hypr/hyprland/binding.lua
@@ -13,7 +13,7 @@ local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 hl.bind(mainMod .. " + T", hl.dsp.exec_cmd(terminal))
 -- local closeWindowBind = hl.bind(mainMod .. " + TAB + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
-hl.bind(mainMod .. " + ALT + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+hl.bind(mainMod .. " + ALT + M", hl.dsp.exec_cmd("command -v uwsm >/dev/null 2>&1 && uwsm stop || loginctl terminate-user ''"))
 hl.bind(mainMod .. " + ALT + L", hl.dsp.exec_cmd("1password --lock ; hyprlock"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
